842c4854b82ef1e8c722461b154c1a94e75f04a2
codepriest <codepriest.gudana@gmail.com>
2021-11-30 12:40:26 +0100

    first commit

--- src/main/java/com/andela/interview/repository/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.andela.interview.repository;
-
-import com.andela.interview.entities.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-
-}
